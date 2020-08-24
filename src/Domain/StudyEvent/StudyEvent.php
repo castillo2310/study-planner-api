808a97a0c299f015202536e2a9b44721ee298a76
Cristian Castillo <castillo2310@gmail.com>
2020-08-24 16:26:15 +0200

    Added color property to StudyEvent class.

--- src/Domain/StudyEvent/StudyEvent.php
+++ src/Domain/StudyEvent/StudyEvent.php
@@ -8,21 +8,24 @@ use DateTime;
 
 class StudyEvent
 {
-    private DateTime $date;
+    private DateTime $date; //TODO: VALUE OBJECTS
     private int $hours;
     private string $description;
+    private string $color;
 
     /**
      * StudyEvent constructor.
      * @param DateTime $date
      * @param int $hours
      * @param string $description
+     * @param string $color
      */
-    public function __construct(DateTime $date, int $hours, string $description)
+    public function __construct(DateTime $date, int $hours, string $description, string $color)
     {
         $this->date = $date;
         $this->hours = $hours;
         $this->description = $description;
+        $this->color = $color;
     }
 
     /**
@@ -47,5 +50,13 @@ class StudyEvent
     public function getDescription(): string
     {
         return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor(): string
+    {
+        return $this->color;
     }
 }
